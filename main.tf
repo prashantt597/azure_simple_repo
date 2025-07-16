c313a7de8c47ad9e724c9ff85fb3b51af75873e5
Umeshdk07 <umeshrdk1997@gmail.com>
2025-07-16 23:28:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "20_04-lts"
+    sku       = "22_04-lts"
     version   = "latest"
   }
 }
