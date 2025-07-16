98034b467abdb7982148817e7b5f8ae250b22ba3
Umeshdk07 <umeshrdk1997@gmail.com>
2025-07-16 23:23:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "azurerm_public_ip" "public_ip" {
   name                = "simple-vm-public-ip"
   location            = "East US"
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 resource "azurerm_network_interface" "nic" {
